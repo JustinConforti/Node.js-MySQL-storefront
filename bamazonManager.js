f82e60155bde5bdac130fe5879d682478bd5ae8b
Justin <justinconforti@gmail.com>
2019-07-08 22:47:19 -0400

    created a switch case and functions for the four manager options

--- bamazonManager.js
+++ bamazonManager.js
@@ -19,6 +19,7 @@
 
 var mysql = require("mysql");
 var inquirer = require("inquirer")
+var Table = require('easy-table')
 
 
 var connection = mysql.createConnection({
@@ -53,6 +54,79 @@ function displayOptions() {
     ])
 .then(function(inquirerResponse) {
 
-    console.log(inquirerResponse)
+    let response = inquirerResponse.listedOptions
+    console.log(response.toString())
+
+    switch(response) {
+        case ("View Products for Sale"):
+        fullProducts()
+        break;
+        case ("View Low Inventory"):
+        lowInventory()
+        break;
+        case ("Add to Inventory"):
+        addInventory()
+        break;
+        case ("Add New Product"):
+        newProduct()
+        break;
+    }
+
 })
+}
+function askPrompt() {
+    inquirer
+.prompt([{
+    type: "list",
+    message: "What would you like to do from here?",
+    choices: ["Quit", "Another Option"],
+    name: "listedOptions"
+}
+])
+.then(function(inquirerResponse) {
+    if (inquirerResponse.listedOptons === "Quit") {
+        connection.end();
+    } else {
+        displayOptions()
+    }
+})
+
+}
+function fullProducts() {
+    connection.query("SELECT * FROM products", function (err, result) {
+        if (err) throw err;
+        let fullItemArray = [];
+    
+        for (let i = 0; i < result.length; i++) {
+          let itemArray = {
+            id: result[i].item_id,
+            name: result[i].product_name,
+            price: result[i].price,
+            quantity: result[i].stock_quantity
+          }
+          fullItemArray.push(itemArray)
+        }
+        var t = new Table
+        fullItemArray.forEach(function (product) {
+          t.cell('Product Id', product.id)
+          t.cell('name', product.name)
+          t.cell('Price, USD', product.price, Table.number(2))
+          t.cell('quantity', product.quantity)
+          t.newRow()
+        })
+    
+        console.log(t.toString())
+})
+}
+
+function lowInventory() {
+    console.log("low invetory")
+}
+
+function addInventory() {
+    console.log("add invetory")
+}
+
+function newProduct() {
+    console.log("new product")
 }
